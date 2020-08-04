adcf955b8321cf4cbf118e5b2531c055707c2af9
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-08-04 01:53:09 -0300

    added new routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,29 @@
 import { Router } from 'express';
 
 import userController from './Controllers/userController';
+
 import loginController from './Controllers/loginController';
+
+import naverController from './Controllers/naverController';
+
+import projectController from './Controllers/projectController';
+
+import authMidleware from './Midlewares/userAuth';
 
 const routes = new Router();
 
 routes.post('/signup', userController.store);
 routes.post('/login', loginController.store);
+
+routes.use(authMidleware);
+
+routes.post('/navers', naverController.store);
+routes.get('/navers', naverController.index);
+routes.get('/navers/:id', naverController.show);
+routes.delete('/navers/:id', naverController.delete);
+
+routes.get('/projects', projectController.index);
+routes.get('/projects', projectController.show);
+routes.post('/projects', projectController.Store);
 
 export default routes;
