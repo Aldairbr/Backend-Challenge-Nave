30045b8b874386552a86d6107960ab4e0bd96a74
Aldair Rodrigues <ifsulaldair@gmail.com>
2020-08-05 18:58:17 -0300

    fix/ added new routes

--- src/routes.js
+++ src/routes.js
@@ -12,18 +12,21 @@ import authMidleware from './Midlewares/userAuth';
 
 const routes = new Router();
 
-routes.post('/signup', userController.store);
-routes.post('/login', loginController.store);
+routes.post('/signup', userController.Store);
+routes.post('/login', loginController.Store);
 
 routes.use(authMidleware);
 
-routes.post('/navers', naverController.store);
-routes.get('/navers', naverController.index);
-routes.get('/navers/:id', naverController.show);
-routes.delete('/navers/:id', naverController.delete);
+routes.get('/navers', naverController.Index);
+routes.post('/navers', naverController.Store);
+routes.get('/navers/:id', naverController.Show);
+routes.delete('/navers/:id', naverController.Delete);
+// routes.put('/navers/:id', naverController.Update);
 
-routes.get('/projects', projectController.index);
-routes.get('/projects', projectController.show);
+routes.get('/projects', projectController.Index);
 routes.post('/projects', projectController.Store);
+routes.get('/projects/:id', projectController.Show);
+routes.delete('/projects/:id', projectController.Delete);
+// routes.put('/projects/:id', projectController.Update);
 
 export default routes;
